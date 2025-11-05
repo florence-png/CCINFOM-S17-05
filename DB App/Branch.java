645614c3f5dbfb229e5a1c31de07b40ce7858beb
cikat67 <asia_teo@dlsu.edu.ph>
2025-11-05 12:34:46 +0800

    Uploaded Branch model

---
+++ DB App/Branch.java
@@ -0,0 +1,29 @@
+public class Branch {
+    private int branchId;
+    private String name;
+    private String location;
+    private String contactNumber;
+
+    public Branch(int branchId, String name, String location, String contactNumber) {
+        this.branchId = branchId;
+        this.name = name;
+        this.location = location;
+        this.contactNumber = contactNumber;
+    }
+
+    public int getBranchId() {
+        return branchId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+}
