ecac84225a408c824ee0bfefc9ec63afe48c5613
HiddenGold <104958576+HiddenG0ld@users.noreply.github.com>
2025-11-19 19:51:46 +0800

    Modify addHospital to include region and ID generation
    Updated the addHospital method to include region and generate hospital_id.

--- src/HospitalDAO.java
+++ src/HospitalDAO.java
@@ -5,18 +5,27 @@ import java.util.List;
 public class HospitalDAO {
     // Add new hospital
     public void addHospital(String hospitalName, String street,
-                               String city, String contactNumber) {
-        String sql = "INSERT INTO hospitals (name, street, city, contact_number, status)" +
-                     "VALUES (?, ?, ?, ?, ?)";
+                               String city, String region, String contactNumber) {
+        String sql = "INSERT INTO hospitals (hospital_id, hospital_name, street, city, region, contact_number, status)" +
+                     "VALUES (?, ?, ?, ?, ?, ?, ?)";
 
         try(Connection conn = DBConnector.getConnection();
             PreparedStatement stmt = conn.prepareStatement(sql)){
 
-            stmt.setString(1, hospitalName);
-            stmt.setString(2, street);
-            stmt.setString(3, city);
-            stmt.setString(4, contactNumber);
-            stmt.setString(5, "Operational");
+        	int nextId = 0;
+            String query = "SELECT MAX(technician_id) FROM technicians";
+            ResultSet rs = stmt.executeQuery(query);
+            if(rs.next()){
+                nextId = rs.getInt(1) + 1;
+            }
+            
+            stmt.setInt(1, nextId);
+            stmt.setString(2, hospitalName);
+            stmt.setString(3, street);
+            stmt.setString(4, city);
+            stmt.setString(5, region);
+            stmt.setString(6, contactNumber);
+            stmt.setString(7, "Operational");
 
             stmt.executeUpdate();
             System.out.println("Hospital added successfully!");
